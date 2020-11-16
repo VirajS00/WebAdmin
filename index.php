20bab530e0845665d9c9c6414689a4258690c752
VirajS00 <virajshukla00@gmail.com>
2020-11-17 00:05:03 +0530

    added menu

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
 		<link rel="stylesheet" href="css/style.css" />
 	</head>
 	<body>
+		<div class="nav-container">
+			<nav>
+				<ul class="nav-ul">
+					<li class="nav-li"><h2 class="head-menu">Menu</h2></li>
+					<li class="nav-li"><a href="#" class="link-menu">Add Photos</a></li>
+					<li class="nav-li"><a href="#" class="link-menu">Other content</a></li>
+					<li class="nav-li"><a href="#" class="link-menu">Feedback</a></li>
+					<li class="nav-li"><a href="#" class="link-menu">Logout</a></li>
+				</ul>
+			</nav>
+		</div>
 		<main>
 		<img src="images/menu.svg" alt="menu sign" class="menuSign" height="30px" width="30px">
 			<div class="chart_container">
@@ -29,6 +40,7 @@
 		</main>
 
 		<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-		<script src="js/chartTest.js"></script>
+		<script src="js/charts.js"></script>
+		<script src="js/nav.js"></script>
 	</body>
 </html>
---
+++ js/nav.js
@@ -0,0 +1,15 @@
+const menuSign = document.querySelector('.menuSign');
+const container = document.querySelector('.nav-container');
+const menu = document.querySelector('.nav-ul');
+menuSign.addEventListener('click', () => {
+	menu.style.transform = 'scale(1, 1)';
+	container.style.pointerEvents = 'fill';
+	container.style.transform = 'scale(1, 1)';
+});
+
+container.addEventListener('click', (e) => {
+	if (e.target !== container) return;
+	menu.style.transform = 'scale(0, 1)';
+	container.style.pointerEvents = 'none';
+	container.style.transform = 'scale(0, 1)';
+});
